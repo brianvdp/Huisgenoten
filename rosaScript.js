47c9c2801d4b700247ff13d2b1668a85309244e2
Brian <brianvdput@gmail.com>
2022-06-21 21:36:38 +0200

    tabs

---
+++ rosaScript.js
@@ -0,0 +1,8 @@
+function openShowcase(ShowcaseNumber) {
+    var i;
+    var x = document.getElementsByClassName("Showcase");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    document.getElementById(ShowcaseNumber).style.display = "block";  
+  }
