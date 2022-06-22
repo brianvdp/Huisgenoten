e1d8ab0a435aa872c7dfb3fff2f4bbccf0270804
Brian <brianvdput@gmail.com>
2022-06-22 13:14:20 +0200

    tabs

--- myScript.js
+++ myScript.js
@@ -1,17 +1,8 @@
-var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
-var tabPanels=document.querySelectorAll(".tabContainer .tabPanel ");
-
-function showPanel(panelIndex,colorCode) {
-    tabButtons.forEach(function(node){
-        node.style.backgroundColor="#139A43";
-        node.style.color="white";
-    });
-    
-    tabButtons[panelIndex].style.color="white";
-    tabButtons[panelIndex].style.backgroundColor="#139A43";
-    tabPanels.forEach(function(node){
-        node.style.display="none";
-    });
-    tabPanels[panelIndex].style.display="block";
-}
-showPanel(0);
+function openShowcase(ShowcaseNumber) {
+    var i;
+    var x = document.getElementsByClassName("Showcase");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    document.getElementById(ShowcaseNumber).style.display = "block";  
+  }
